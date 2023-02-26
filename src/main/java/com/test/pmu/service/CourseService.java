3dd24ba23b5652390d3424519b1a91a6fc9016ca
zaerbib <ilunga.joel@yahoo.fr>
2023-02-26 15:34:47 +0100

    * add kafka * add postgresql docker compose * add repository layer test

--- src/main/java/com/test/pmu/service/CourseService.java
+++ src/main/java/com/test/pmu/service/CourseService.java
@@ -1,11 +1,20 @@
 package com.test.pmu.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.pmu.entity.Course;
 import com.test.pmu.entity.Partant;
 import com.test.pmu.event.EventCourse;
 import com.test.pmu.exception.PmuException;
 import com.test.pmu.repository.CourseRepository;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -13,13 +22,22 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class CourseService {
 
     private final CourseRepository courseRepository;
+    private final KafkaTemplate<String, EventCourse> kafkaTemplate;
 
-    public CourseService(CourseRepository courseRepository) {
+    public CourseService(CourseRepository courseRepository, KafkaTemplate<String, EventCourse> kafkaTemplate) {
         this.courseRepository = courseRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
+
+    @Value("${microservice.kafka.topics.course-event-store}")
+    private String topicName;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     /**
      * save course service
@@ -32,6 +50,7 @@ public class CourseService {
             PmuException exception = PmuException.builder().message("Course existe déjà pour aujourd'hui").build();
             EventCourse eventCourse = EventCourse.builder().id(UUID.randomUUID().toString())
                     .date(LocalDate.now()).course(course).exception(exception).build();
+            sendEventcourse(eventCourse);
             throw exception;
         }
 
@@ -40,19 +59,40 @@ public class CourseService {
                 PmuException exception = PmuException.builder().message("les partants ne sont pas correctement numérotés").build();
                 EventCourse eventCourse = EventCourse.builder().id(UUID.randomUUID().toString())
                         .date(LocalDate.now()).course(course).exception(exception).build();
+                sendEventcourse(eventCourse);
                 throw exception;
             }
             if(!isFirstNumberOne(course.getPartants())) {
                 PmuException exception = PmuException.builder().message("Aucun partant n'a le numéro 1").build();
                 EventCourse eventCourse = EventCourse.builder().id(UUID.randomUUID().toString())
                         .date(LocalDate.now()).course(course).exception(exception).build();
+                sendEventcourse(eventCourse);
                 throw exception;
             }
         }
         Course result = courseRepository.save(course);
         EventCourse eventCourse = EventCourse.builder().id(UUID.randomUUID().toString())
                                     .date(LocalDate.now()).course(course).course(result).build();
+        sendEventcourse(eventCourse);
         return result;
+    }
+
+    private void sendEventcourse(EventCourse eventCourse) {
+        ListenableFuture<SendResult<String, EventCourse>> future = kafkaTemplate.send(topicName, eventCourse);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, EventCourse>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send message due to {}", ex.getMessage());
+            }
+
+            @SneakyThrows
+            @Override
+            public void onSuccess(SendResult<String, EventCourse> result) {
+                log.info("Sent message=[ {} ], with offset=[{}] ",
+                        objectMapper.writeValueAsString(eventCourse), result.getProducerRecord());
+            }
+        });
     }
 
     /**
